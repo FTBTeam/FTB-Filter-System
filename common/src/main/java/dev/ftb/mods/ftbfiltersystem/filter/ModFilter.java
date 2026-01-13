de40d1eb7c2a6dc96f2c13110625ba841f073534
furglitch <samsonger@proton.me>
2026-01-12 22:56:17 -0600

    feat: allow wildcards in expression mod()

--- common/src/main/java/dev/ftb/mods/ftbfiltersystem/filter/ModFilter.java
+++ common/src/main/java/dev/ftb/mods/ftbfiltersystem/filter/ModFilter.java
@@ -4,6 +4,7 @@ import dev.ftb.mods.ftbfiltersystem.api.FTBFilterSystemAPI;
 import dev.ftb.mods.ftbfiltersystem.api.filter.AbstractSmartFilter;
 import dev.ftb.mods.ftbfiltersystem.api.filter.SmartFilter;
 import net.minecraft.core.HolderLookup;
+import java.util.regex.Pattern;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
 
@@ -11,6 +12,8 @@ public class ModFilter extends AbstractSmartFilter {
     public static final ResourceLocation ID = FTBFilterSystemAPI.rl("mod");
 
     private final String modId;
+    private final String patternArg;
+    private final Pattern patternRegex;
 
     public ModFilter(SmartFilter.Compound parent) {
         this(parent, "minecraft");
@@ -18,8 +21,17 @@ public class ModFilter extends AbstractSmartFilter {
 
     public ModFilter(SmartFilter.Compound parent, String modId) {
         super(parent);
-
         this.modId = modId;
+        this.patternArg = null;
+        this.patternRegex = null;
+    }
+
+    private ModFilter(SmartFilter.Compound parent, String patternArg, Pattern patternRegex) {
+        super(parent);
+
+        this.modId = "";
+        this.patternArg = patternArg;
+        this.patternRegex = patternRegex;
     }
 
     @Override
@@ -29,15 +41,37 @@ public class ModFilter extends AbstractSmartFilter {
 
     @Override
     public boolean test(ItemStack stack) {
-        return stack.getItem().arch$registryName().getNamespace().equals(modId);
+        String ns = stack.getItem().arch$registryName().getNamespace();
+        if (patternRegex != null) {
+            return patternRegex.matcher(ns).matches();
+        }
+        return ns.equals(modId);
     }
 
     @Override
     public String getStringArg(HolderLookup.Provider registryAccess) {
-        return modId;
+        return patternArg == null ? modId : patternArg;
     }
 
     public static ModFilter fromString(SmartFilter.Compound parent, String str, HolderLookup.Provider ignored2) {
+        if (str.indexOf('*') >= 0) {
+            StringBuilder sb = new StringBuilder();
+            sb.append('^');
+            for (int i = 0; i < str.length(); i++) {
+                char c = str.charAt(i);
+                if (c == '*') {
+                    sb.append(".*");
+                } else if (".\\+?^${}()|[]".indexOf(c) >= 0) {
+                    sb.append('\\').append(c);
+                } else {
+                    sb.append(c);
+                }
+            }
+            sb.append('$');
+            Pattern p = Pattern.compile(sb.toString());
+            return new ModFilter(parent, str, p);
+        }
+
         return new ModFilter(parent, str);
     }
 }
