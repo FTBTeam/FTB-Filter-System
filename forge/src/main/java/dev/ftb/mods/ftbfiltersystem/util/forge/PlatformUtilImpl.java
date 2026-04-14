a36ac1fd043a18c474412c6cafbce80da25a0979
Michael Hillcox <michaelhillcox@gmail.com>
2026-04-14 12:46:14 +0100

    chore: remove forge

--- forge/src/main/java/dev/ftb/mods/ftbfiltersystem/util/forge/PlatformUtilImpl.java
+++
@@ -1,9 +0,0 @@
-package dev.ftb.mods.ftbfiltersystem.util.forge;
-
-import net.minecraft.world.item.ItemStack;
-
-public class PlatformUtilImpl {
-    public static int getFoodValue(ItemStack stack) {
-        return stack.getItem().isEdible() ? stack.getItem().getFoodProperties(stack, null).getNutrition() : 0;
-    }
-}
