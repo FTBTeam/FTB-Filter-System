b8e5caa737e591bb21529ad31eabc1fa2ef29a1c
Des Herriott <des.herriott@gmail.com>
2026-06-22 11:43:56 +0100

    build: version -> 26.1.2.2

---
+++ fabric/src/main/java/dev/ftb/mods/ftbfiltersystem/api/fabric/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package dev.ftb.mods.ftbfiltersystem.api.fabric;
+
+import org.jspecify.annotations.NullMarked;
---
+++ fabric/src/main/java/dev/ftb/mods/ftbfiltersystem/fabric/mixin/package-info.java
@@ -0,0 +1,4 @@
+@NullMarked
+package dev.ftb.mods.ftbfiltersystem.fabric.mixin;
+
+import org.jspecify.annotations.NullMarked;
